f6076f1445053e8c9ec5455623b67ee5be3a270d
ph <ph@flashapp.com.br>
2021-12-13 21:58:53 -0300

    arrumaçãozinha

--- src/quadratura_gauss.js
+++ src/quadratura_gauss.js
@@ -1,56 +1,46 @@
 const { GAUSS_CONSTANTS } = require('./constants')
 
-// adaptar pra receber n !== [6,8,10] & receber c[] (w_j = peso) e r[] (x_j = nome)
 /**
  * Calcula integral usando quadratura de gauss
  * 
  * @param {Number} a limite inferior de x
  * @param {Number} b limite superior de x
- * @param {Number} n numero de subintervalos
+ * @param {Number} num_subintervalos numero de subintervalos
  * @param {Function} funcao_a_integrar função a ser integrada
- * @param {Array<Number>} [pesos] lista injetada de pesos
+ * @param {Array<Number>} [coeficientes] lista injetada de pesos
  * @param {Array<Number>} [raizes] lista injetada de raizes
  * 
  * @returns {Number} resultado da aprox. de gauss de uma integral
  */
-function quadratura_gauss(a, b, n, funcao_a_integrar, pesos = null, raizes = null) {
+function quadratura_gauss(a, b, num_subintervalos, funcao_a_integrar, coeficientes = null, raizes = null) {
   const pontos_a_percorrer = [a]
-  const largura_passo = (b - a) / n
+  const largura_passo = (b - a) / num_subintervalos
   let resultado_final = 0
-  let coef_legendre = [5 / 9, 8 / 9, 5 / 9]
-  let raiz_legendre = [-Math.sqrt(0.6), 0, Math.sqrt(0.6)]
+  let coef_legendre = coeficientes
+  let raiz_legendre = raizes
 
-  // if (n === 3) {
-  //   coef_legendre = [5 / 9, 8 / 9, 5 / 9]
-  //   raiz_legendre = [-Math.sqrt(0.6), 0, Math.sqrt(0.6)]
-  // }
-  // if (n === 6) {
-  //   coef_legendre = GAUSS_CONSTANTS.n6.w
-  //   raiz_legendre = GAUSS_CONSTANTS.n6.x
-  // }
-  // if (n === 8) {
-  //   coef_legendre = GAUSS_CONSTANTS.n8.w
-  //   raiz_legendre = GAUSS_CONSTANTS.n8.x
-  // }
-  // if (n === 10) {
-  //   coef_legendre = GAUSS_CONSTANTS.n10.w
-  //   raiz_legendre = GAUSS_CONSTANTS.n10.x
-  // }
-  // if (pesos && raizes) {
-  //   coef_legendre = pesos
-  //   raiz_legendre = raizes
-  // }
+  if (!coeficientes && !raizes) {
+    const sorteio = Math.floor(Math.random() * GAUSS_CONSTANTS.length)
+    const constants = GAUSS_CONSTANTS[sorteio]
+    console.log(`Usando dados do polinômio de Legendre de grau ${constants.grau}`)
+
+    coef_legendre = constants.coeficientes
+    raiz_legendre = constants.raizes
+  }
 
   // prepara os pontos a percorrer
-  for (let j = 1; j < n + 1; j++) {
+  for (let j = 1; j < num_subintervalos + 1; j++) {
     pontos_a_percorrer[j] = pontos_a_percorrer[j - 1] + largura_passo
   }
 
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < num_subintervalos; i++) {
     const q_1 = (pontos_a_percorrer[i + 1] - pontos_a_percorrer[i]) / 2
     const q_2 = (pontos_a_percorrer[i + 1] + pontos_a_percorrer[i]) / 2
 
-    const raizes_ajustadas = raiz_legendre.map((raiz_atual) => q_1 * raiz_atual + q_2)
+    let raizes_ajustadas = []
+    for (let i = 0; i < raiz_legendre.length; i++) {
+      raizes_ajustadas[i] = q_1 * raiz_legendre[i] + q_2
+    }
 
     let resultado_parcial = 0
     for (let i = 0; i < coef_legendre.length; i++) {
