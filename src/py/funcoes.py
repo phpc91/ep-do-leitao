e746b3faec316174dcfccce4810def7954c30eef
ph <ph@flashapp.com.br>
2021-12-20 00:09:07 -0300

    código em python

---
+++ src/py/funcoes.py
@@ -0,0 +1,9 @@
+import math
+
+
+def x_ao_quadrado(x, y):
+    return x * x
+
+
+def y_2_log10_3x(x, y):
+    return y * y * math.log10(3 * x)
---
+++ src/py/integral_dupla_gl.py
@@ -0,0 +1,75 @@
+import funcoes
+import constantes
+
+
+def integral_dupla_gauss_legendre(
+    ax,
+    bx,
+    ay,
+    by,
+    coeficientes_x,
+    raizes_x,
+    coeficientes_y,
+    raizes_y,
+    funcao_a_integrar
+):
+    """
+    ax, bx são números; são os limites em x da integral
+    ay, by podem ser números ou funções em x; são os limites em y da integral
+    coeficientes_x são os pesos do polinômio de legendre usado no eixo X
+    raizes_x são as raízes do polinômio de legendre usado no eixo X
+    coeficientes_y são os pesos do polinômio de legendre usado no eixo Y
+    raizes_y são as raízes do polinômio de legendre usado no eixo Y
+    funcao_a_integrar é a sobre a qual queremos calcular a integral dupla
+    """
+
+    a = ax
+    b = bx
+    c = ay
+    d = by
+    soma_x_y = 0
+
+    # loop em X
+    for i in range(len(raizes_x)):
+        A_i = coeficientes_x[i]
+        qx_1 = (b - a) / 2
+        qx_2 = (b + a) / 2
+        x_i = qx_1 * raizes_x[i] + qx_2  # ajuste de variavel
+
+        if (callable(ay)):
+            c = ay(x_i)
+        if (callable(by)):
+            d = by(x_i)
+
+        qy_1 = (d - c) / 2
+        qy_2 = (d + c) / 2
+
+        soma_y = 0
+
+        # loop em Y
+        for j in range(len(raizes_y)):
+            B_j = coeficientes_y[j]
+            y_j = qy_1 * raizes_y[j] + qy_2  # ajuste de variavel
+            resultado_funcao = funcao_a_integrar(x_i, y_j)
+            soma_y += B_j * resultado_funcao  # somatório de B[i] * f(x,y)
+
+        # somatório de A[i] * resultado em Y * correção da troca de variável
+        soma_x_y += A_i * soma_y * (d - c) * 0.5
+
+    # resultado final é soma_x_y * correção da troca de variável
+    integral_dupla = 0.5 * (b - a) * soma_x_y
+    print(integral_dupla)
+    return integral_dupla
+
+
+integral_dupla_gauss_legendre(
+    1,  # a
+    4,  # b
+    0,  # c
+    2,  # d
+    constantes.LEGENDRE.get('grau6').get('coeficientes'),  # coefs_x
+    constantes.LEGENDRE.get('grau6').get('raizes'),  # raizes_x
+    constantes.LEGENDRE.get('grau6').get('coeficientes'),  # coefs_y
+    constantes.LEGENDRE.get('grau6').get('raizes'),  # raizes_y
+    funcoes.y_2_log10_3x,  # funcao_a_integrar
+)
